9f8644ab8b8d27c99fcfbc59aae7f3ad4ce88abd
vsoch <vsochat@stanford.edu>
2016-12-15 14:51:58 -0800

    don't want to rename index :)

--- circle_urls.py
+++ circle_urls.py
@@ -5,6 +5,7 @@ circle_urls.py will rename all url files to not have extension .html
 '''
 import sys
 import os
+import re
 from glob import glob
 
 site_dir = os.path.abspath(sys.argv[1])
@@ -12,6 +13,7 @@ print("Using site directory %s" %(site_dir))
 
 files = glob("%s/*.html" %(site_dir))
 for html_file in files:
-    new_name = html_file.strip('.html')
-    print("Renaming %s to %s." %(html_file,new_name))
-    os.rename(html_file, new_name)
+    if not html_file.endswith('index.html'):
+        new_name = html_file.replace('.html','')
+        print("Renaming %s to %s." %(html_file,new_name))
+        os.rename(html_file, new_name)
